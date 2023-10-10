5040645230aa34c669074d009ab5c16b188166c0
ALINY OLIVEIRA DA SILVA <aliny.osilva1@senacsp.edu.br>
2023-10-09 22:21:18 -0300

    a

---
+++ is_aulas/02/js/script.js
@@ -0,0 +1,88 @@
+console.log(2);
+console.log(10);
+console.log("Meu nome é Aliny");
+console.log(5+1);
+console.log(typeof 2);
+console.log(typeof 10);
+console.log(typeof "Meu nome é Aliny");
+console.log(typeof 5+1);
+
+// comentário de linha única
+
+/** Comentário 
+    longo
+ */
+
+
+
+// Operações matemáticas
+console.log(2+4);
+console.log(10-5);
+console.log(5*4);
+console.log(20/4);
+
+// Texto
+console.log("Eu sou um texto")
+console.log('Eu sou um texto')
+console.log(`Eu sou um texto`)
+
+console.log(typeof "Eu sou um texto")
+console.log(typeof 'Eu sou um texto')
+console.log(typeof `Eu sou um texto`)
+
+console.log("Testando \n quebra de linha");
+console.log("Testando \t quebra de linha");
+
+// concatenação
+
+console.log("Oi")
+console.log("Tudo bem")
+
+console.log("oi, " + "tudo bem?")
+console.log("A soma de 2 + 2 = 4")
+console.log("A soma é: " + (2+2))
+
+// Interpolação
+console.log(`A soma é: ${2+2}`)
+
+// Boolean
+console.log(5>3)
+console.log(3>5)
+console.log(5<3)
+console.log(3 >= 5)
+console.log(3 <= 5)
+console.log(5 == 5)
+console.log(5 != 5)
+console.log(5 != 4)
+
+let nome = "Aliny";
+console.log(nome)
+
+nome = "teacher Aliny";
+
+console.log(nome);
+
+let idade = 37;
+console.log(idade);
+
+idade = idade + 1;
+console.log(idade);
+idade += 1; //Incrementar
+console.log(idade)
+idade -= 1; //decrementar
+console.log(idade)
+
+let nome1 = "Guilherme"
+console.log(nome1)
+
+// alert("Seja bem vindo, jovem!")
+
+// alert (`O nome é ${nome1}`)
+
+nome1 = "bb"
+// alert (`O nome é ${nome1}`)
+
+const nomeCompleto = "Aliny O. S."
+console.log(nomeCompleto)
+
+nomeCompleto = "Aliny Oliveira da Silva"
