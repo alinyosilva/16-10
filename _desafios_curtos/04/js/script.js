f6e755422fca2813a258b19df4ca83893265cd36
ALINY OLIVEIRA DA SILVA <aliny.osilva1@senacsp.edu.br>
2023-10-16 22:10:25 -0300

    a

---
+++ _desafios_curtos/04/js/script.js
@@ -0,0 +1,51 @@
+// let nota1 = parseFloat(prompt("nota01"))
+// let nota2 = parseFloat(prompt("nota02"))
+// let nota3 = parseFloat(prompt("nota03"))
+
+
+// let media = (nota1 + nota2 + nota3) / 3
+
+// if(media < 5){
+//     console.log(`Aluno(a) reprovado(a) com média ${media}`)
+// } else if(media >= 5 && media < 7) {
+//     console.log(`Aluno(a) em recuperação com média ${media}`)
+// } else {
+//     console.log(`Aluno(a) aprovado(a) com média ${media}`)
+// }
+
+
+let mes = prompt("Digite um número de 1 a 12:")
+
+if(mes == 1){
+    console.log("Janeiro")
+} else if(mes == 2) {
+    console.log("Fevereiro")
+} else if(mes == 3) {
+    console.log("Março")
+} else if(mes == 4) {
+    console.log("Abril")
+} else if(mes == 5) {
+    console.log("Maio")
+} else if(mes == 6) {
+    console.log("Junho")
+} else if(mes == 7) {
+    console.log("Julho")
+} else if(mes == 8) {
+    console.log("Agosto")
+} else if(mes == 9) {
+    console.log("Setembro")
+} else if(mes == 10) {
+    console.log("Outubro")
+} else if(mes == 11) {
+    console.log("Novembro")
+} else if(mes == 12) {
+    console.log("Dezembro")
+} else {
+    console.log("O ano só tem 12 meses ._.")
+}
+
+
+
+
+
+
