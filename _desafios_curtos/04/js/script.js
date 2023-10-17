284d6d1f020097cbbf7da4de408506e73d99a70e
ALINY OLIVEIRA DA SILVA <aliny.osilva1@senacsp.edu.br>
2023-10-16 22:23:59 -0300

    a

--- _desafios_curtos/04/js/script.js
+++ _desafios_curtos/04/js/script.js
@@ -14,38 +14,81 @@
 // }
 
 
+
+
+
+
+
+// let mes = prompt("Digite um número de 1 a 12:")
+
+// if(mes == 1){
+//     console.log("Janeiro")
+// } else if(mes == 2) {
+//     console.log("Fevereiro")
+// } else if(mes == 3) {
+//     console.log("Março")
+// } else if(mes == 4) {
+//     console.log("Abril")
+// } else if(mes == 5) {
+//     console.log("Maio")
+// } else if(mes == 6) {
+//     console.log("Junho")
+// } else if(mes == 7) {
+//     console.log("Julho")
+// } else if(mes == 8) {
+//     console.log("Agosto")
+// } else if(mes == 9) {
+//     console.log("Setembro")
+// } else if(mes == 10) {
+//     console.log("Outubro")
+// } else if(mes == 11) {
+//     console.log("Novembro")
+// } else if(mes == 12) {
+//     console.log("Dezembro")
+// } else {
+//     console.log("O ano só tem 12 meses ._.")
+// }
+
+
 let mes = prompt("Digite um número de 1 a 12:")
 
-if(mes == 1){
-    console.log("Janeiro")
-} else if(mes == 2) {
-    console.log("Fevereiro")
-} else if(mes == 3) {
-    console.log("Março")
-} else if(mes == 4) {
-    console.log("Abril")
-} else if(mes == 5) {
-    console.log("Maio")
-} else if(mes == 6) {
-    console.log("Junho")
-} else if(mes == 7) {
-    console.log("Julho")
-} else if(mes == 8) {
-    console.log("Agosto")
-} else if(mes == 9) {
-    console.log("Setembro")
-} else if(mes == 10) {
-    console.log("Outubro")
-} else if(mes == 11) {
-    console.log("Novembro")
-} else if(mes == 12) {
-    console.log("Dezembro")
-} else {
-    console.log("O ano só tem 12 meses ._.")
+switch (mes) {
+    case 1:
+        console.log ("Janeiro")
+        break;
+    case 2:
+        console.log ("Fevereiro")
+        break;
+    case 3:
+        console.log ("Março")
+        break;
+    case 4:
+        console.log ("Abril")
+        break;
+    case 5:
+        console.log ("Maio")
+        break;
+    case 6:
+        console.log ("")
+        break;
+    case 7:
+        console.log ("")
+        break;
+    case 8:
+        console.log ("")
+        break;
+    case 9:
+        console.log ("")
+        break;
+    case 10:
+        console.log ("")
+        break;
+
+
+
+
+    default:
+        console.log ("Mês inválido")
+
+        
 }
-
-
-
-
-
-
